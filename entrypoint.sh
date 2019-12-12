7f85da73c58e5fc3c522f2d322734c1925716f3a
Herbert Knapp <herbert.knapp@wirecard.com>
2019-12-12 11:02:29 +0100

    Abort on error, remove typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,25 @@
 
 _error() {
     echo "::error file=entrypoint.sh::$1"
+    ERRORS=true
 }
 
 _pre() {
-    if [[ -z $INPUT_AWS_ACCESS_KEY_ID ]]; then
+    if [[ -z ${INPUT_AWS_ACCESS_KEY_ID} ]]; then
         _error "AWS_ACCESS_KEY_ID not provided."
     fi
-    if [[ -z $INPUT_AWS_SECRET_ACCESS_KEY ]]; then
+    if [[ -z ${INPUT_AWS_SECRET_ACCESS_KEY} ]]; then
         _error "AWS_SECRET_ACCESS_KEY not provided."
     fi
-    if [[ -z $INPUT_AWS_REGION ]]; then
+    if [[ -z ${INPUT_AWS_REGION} ]]; then
         _error "AWS_REGION not provided."
     fi
-    if [[ -z $INPUT_AWS_S3_BUCKET ]]; then
+    if [[ -z ${INPUT_AWS_S3_BUCKET} ]]; then
         _error "AWS_S3_BUCKET not provided."
+    fi
+    if [[ ${ERRORS} == "true" ]]; then
+        echo "Aborting deployment"
+        exit 1
     fi
 }
 
@@ -31,7 +36,7 @@ EOF
 _deploy() {
     aws s3 sync "${INPUT_SRC_DIR:-.}" \
     "s3://${INPUT_AWS_S3_BUCKET}/${INPUT_DST_DIR}" \
-    --profile s3-deploy-actions
+    --profile s3-deploy-action
 }
 
 _run() {
